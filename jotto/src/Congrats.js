83acd095369172107c06b4ef74e31daf98ac05d2
Guilherme Brunner <guilherme.brunner@smarttbotter.com>
2021-04-13 12:12:20 -0300

    Add Bootstrap styling

--- jotto/src/Congrats.js
+++ jotto/src/Congrats.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const Congrats = ({ success }) => {
   return success ? (
-    <div data-test-id="component-congrats">
+    <div data-test-id="component-congrats" className="alert alert-success">
       <span data-test-id="congrats-message">
         Congratulations! You guessed the word!
       </span>
