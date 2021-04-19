d48984187e1e5d37addd8646460596cb6de24341
Guilherme Brunner <guilherme.brunner@smarttbotter.com>
2021-04-19 15:00:17 -0300

    Mocking server call and writing mocked fetching tests for jotto app

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -1,21 +1,26 @@
+import React, { useEffect } from "react";
+
 import Congrats from "./Congrats";
-import Input from './Input';
+import Input from "./Input";
 import GuessedWords from "./GuessedWords";
+import { getSecretWord } from "./actions";
 
 function App() {
   // TODO: get props from shared state
   const success = false;
-  const secretWord = 'party';
+  const secretWord = "party";
   const guessedWords = [];
+
+  useEffect(() => {
+    getSecretWord();
+  }, []);
 
   return (
     <div className="container" data-test-id="app-component">
       <h1>Jotto</h1>
       <Congrats success={success} />
       <Input success={success} secretWord={secretWord} />
-      <GuessedWords
-        guessedWords={guessedWords}
-      />
+      <GuessedWords guessedWords={guessedWords} />
     </div>
   );
 }
